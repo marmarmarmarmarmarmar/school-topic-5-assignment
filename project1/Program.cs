758568449a8de69664f8d35a8d8e23c84f3dad64
yesyes <hydrogenmelon@pm.me>
2024-09-24 09:39:53 -0400

    aaa

---
+++ project1/Program.cs
@@ -0,0 +1,17 @@
+﻿/* 
+Coda | 2024/09/24
+Topic 5 - If Statements - Assignment
+*/
+
+using System;
+namespace ui_tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
+
